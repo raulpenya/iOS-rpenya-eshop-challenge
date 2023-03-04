b859cd4eced7f4d67e3f3b415c2c5800d6a5ca5c
rpenya <rpenya.mail@gmail.com>
2023-03-04 20:02:59 +0100

    ProductsApi done

--- rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductsApi.swift
+++ rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductsApi.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+enum ProductsApi: Api {
+    case getAllProducts
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getAllProducts:
+            return .get
+        }
+    }
+    
+    var url: String {
+        switch self {
+        case .getAllProducts:
+            return "https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json"
+        }
+    }
+    
+    var headerParams: [String: Any] {
+        switch self {
+        case .getAllProducts:
+            return [:]
+        }
+    }
+    
+    var bodyParams: Any {
+        switch self {
+        case .getAllProducts:
+            return [:]
+        }
+    }
+    
+    var useCache: Bool {
+        switch self {
+        case .getAllProducts:
+            return false
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        switch self {
+        case .getAllProducts:
+            return try createURLRequest()
+        }
+    }
+}
