87be2807eacadfe45b259d23e00fb24f15133636
rpenya <rpenya.mail@gmail.com>
2023-03-04 18:21:14 +0100

    initial commit

---
+++ rpenya-eshop-challenge/rpenya-eshop-challenge/rpenya_eshop_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpenya_eshop_challengeApp.swift
+//  rpenya-eshop-challenge
+//
+//  Created by raulbot on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct rpenya_eshop_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
