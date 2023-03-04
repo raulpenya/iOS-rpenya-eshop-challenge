e20078d9f3e79fb4d051862b0219e3f498e8c13d
rpenya <rpenya.mail@gmail.com>
2023-03-04 20:14:09 +0100

    domain repositories methods added

--- rpenya-eshop-challenge/Domain/Products/ProductsRepository.swift
+++ rpenya-eshop-challenge/Domain/Products/ProductsRepository.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol ProductsRepository {
+    func getAllProducts()
+}
--- rpenya-eshop-challenge/Domain/Products/PromotionsRepository.swift
+++ rpenya-eshop-challenge/Domain/Products/PromotionsRepository.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol PromotionsRepository {
+    func getAllPromotions()
+}
