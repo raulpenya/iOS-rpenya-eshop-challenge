9971477d9ae521ae718a3f72d08a830404cc861e
rpenya <rpenya.mail@gmail.com>
2023-03-04 20:14:31 +0100

    ProductRemoteEntity done

--- rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductRemoteEntity.swift
+++ rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductRemoteEntity.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+//{
+//  "products": [
+//    {
+//      "code": "VOUCHER",
+//      "name": "Cabify Voucher",
+//      "price": 5
+//    },
+//    {
+//      "code": "TSHIRT",
+//      "name": "Cabify T-Shirt",
+//      "price": 20
+//    },
+//    {
+//      "code": "MUG",
+//      "name": "Cabify Coffee Mug",
+//      "price": 7.5
+//    }
+//  ]
+//}
+
+struct ProductRemoteEntity: Decodable, Equatable {
+    let code: String
+    let name: String
+    let price: Double
+//    let currency: String = "€"
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.code == rhs.code
+    }
+}
