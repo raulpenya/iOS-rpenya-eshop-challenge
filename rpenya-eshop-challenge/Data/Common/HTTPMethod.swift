74a87ce4990466f45a801693e1712e7a5bcb0335
rpenya <rpenya.mail@gmail.com>
2023-03-04 20:02:02 +0100

    api & httpmethod done

--- rpenya-eshop-challenge/Data/Common/HTTPMethod.swift
+++ rpenya-eshop-challenge/Data/Common/HTTPMethod.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct HTTPMethod: RawRepresentable, Equatable, Hashable {
+    static let get = HTTPMethod(rawValue: "GET")
+    static let post = HTTPMethod(rawValue: "POST")
+    static let put = HTTPMethod(rawValue: "PUT")
+    static let delete = HTTPMethod(rawValue: "DELETE")
+    let rawValue: String
+
+    init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+}
