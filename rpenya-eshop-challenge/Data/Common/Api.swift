f619e8f1d6e74b3877fbd91bfa1db0851c67bca9
rpenya <rpenya.mail@gmail.com>
2023-03-04 20:01:46 +0100

    api and httpmethod files added

---
+++ rpenya-eshop-challenge/Data/Common/Api.swift
@@ -0,0 +1,8 @@
+//
+//  Api.swift
+//  Data
+//
+//  Created by raulbot on 4/3/23.
+//
+
+import Foundation
---
+++ rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductsApi.swift
@@ -0,0 +1,8 @@
+//
+//  ProductsApi.swift
+//  Data
+//
+//  Created by raulbot on 4/3/23.
+//
+
+import Foundation
