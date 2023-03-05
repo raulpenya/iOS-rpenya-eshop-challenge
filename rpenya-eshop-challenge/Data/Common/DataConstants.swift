2e8d21fa90565b19b0f26a1b992aebef2db29aad
rpenya <rpenya.mail@gmail.com>
2023-03-05 18:15:31 +0100

    DataConstants added

---
+++ rpenya-eshop-challenge/Data/Common/DataConstants.swift
@@ -0,0 +1,16 @@
+//
+//  DataConstants.swift
+//  Data
+//
+//  Created by raulbot on 5/3/23.
+//
+
+import Foundation
+
+//https://gist.github.com/raulpenya/bd43e4cabd6afbb95ca965eb684fce4a
+
+struct DataConstants {
+    static let baseUrl = "https://gist.githubusercontent.com/"
+    static let getProductsEndpoint = "palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json"
+    static let getPromotionsEndpoint = "raulpenya/bd43e4cabd6afbb95ca965eb684fce4a/raw/8e8e229fb033f694834f04af902f2735d37124b7/Promotions.json"
+}
--- rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductsApi.swift
+++ rpenya-eshop-challenge/Data/Products/DataSource/Remote/Model/ProductsApi.swift
@@ -20,7 +20,7 @@ enum ProductsApi: Api {
     var url: String {
         switch self {
         case .getAllProducts:
-            return "https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json"
+            return DataConstants.baseUrl + DataConstants.getProductsEndpoint
         }
     }
     
