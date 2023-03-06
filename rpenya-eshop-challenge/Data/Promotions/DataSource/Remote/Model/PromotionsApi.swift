4f2d1020c2a13a25604f8078b442751333b1f396
rpenya <rpenya.mail@gmail.com>
2023-03-06 17:09:39 +0100

    PromotionsApi added

---
+++ rpenya-eshop-challenge/Data/Promotions/DataSource/Remote/Model/PromotionsApi.swift
@@ -0,0 +1,54 @@
+//
+//  PromotionsApi.swift
+//  Data
+//
+//  Created by raulbot on 6/3/23.
+//
+
+import Foundation
+
+enum PromotionsApi: Api {
+    case getAllPromotions
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getAllPromotions:
+            return .get
+        }
+    }
+    
+    var url: String {
+        switch self {
+        case .getAllPromotions:
+            return DataConstants.baseUrl + DataConstants.getPromotionsEndpoint
+        }
+    }
+    
+    var headerParams: [String: Any] {
+        switch self {
+        case .getAllPromotions:
+            return [:]
+        }
+    }
+    
+    var bodyParams: Any {
+        switch self {
+        case .getAllPromotions:
+            return [:]
+        }
+    }
+    
+    var useCache: Bool {
+        switch self {
+        case .getAllPromotions:
+            return false
+        }
+    }
+    
+    func asURLRequest() throws -> URLRequest {
+        switch self {
+        case .getAllPromotions:
+            return try createURLRequest()
+        }
+    }
+}
