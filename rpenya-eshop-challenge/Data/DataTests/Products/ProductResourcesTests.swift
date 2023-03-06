a21f4fee215800b3de8ed05539fc100381d16b9b
rpenya <rpenya.mail@gmail.com>
2023-03-06 13:54:27 +0100

    added pending tests

---
+++ rpenya-eshop-challenge/Data/DataTests/Products/ProductResourcesTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProductResourcesTests.swift
+//  rpenya-eshop-challengeTests
+//
+//  Created by raulbot on 6/3/23.
+//
+
+import XCTest
+
+final class ProductResourcesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+}
