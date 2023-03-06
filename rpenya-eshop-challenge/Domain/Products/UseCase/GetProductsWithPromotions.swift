6cdacc117b4ee7579f04f287b5b9b2e333f808d0
rpenya <rpenya.mail@gmail.com>
2023-03-06 21:01:07 +0100

    GetProductsWithPromotions done

--- rpenya-eshop-challenge/Domain/Products/UseCase/GetProductsWithPromotions.swift
+++ rpenya-eshop-challenge/Domain/Products/UseCase/GetProductsWithPromotions.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import Combine
+
+public class GetProductsWithPromotions: UseCase {
+    typealias T = [Product]
+    typealias Q = GetProductsWithPromotionsRequestValues
+    let productsRepository: ProductsRepository
+    let promotionsRepository: PromotionsRepository
+    
+    public init(productsRepository: ProductsRepository, promotionsRepository: PromotionsRepository) {
+        self.productsRepository = productsRepository
+        self.promotionsRepository = promotionsRepository
+    }
+    
+    func execute(_ requestValues: GetProductsWithPromotionsRequestValues) -> AnyPublisher<[Product], Error> {
+        let productPublisher = productsRepository.getAllProducts()
+        let promotionsPublisher = promotionsRepository.getAllPromotions()
+        return Publishers.Zip(productPublisher, promotionsPublisher).flatMap { products, promotion in
+            return Result<[Product], Error>.Publisher(products).eraseToAnyPublisher()
+        }.eraseToAnyPublisher()
+    }
+}
+
+public class GetProductsWithPromotionsRequestValues: RequestValues {}
