e9c4766c102a60499f85fdbb77403832427bf5f6
rpenya <rpenya.mail@gmail.com>
2023-03-06 20:15:25 +0100

    PromotionsRemoteDataSourceTests added

--- rpenya-eshop-challenge/Data/Common/DataConstants.swift
+++ rpenya-eshop-challenge/Data/Common/DataConstants.swift
@@ -12,7 +12,7 @@ import Foundation
 struct DataConstants {
     static let baseUrl = "https://gist.githubusercontent.com/"
     static let getProductsEndpoint = "palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json"
-    static let getPromotionsEndpoint = "raulpenya/bd43e4cabd6afbb95ca965eb684fce4a/raw/8e8e229fb033f694834f04af902f2735d37124b7/Promotions.json"
+    static let getPromotionsEndpoint = "raulpenya/bd43e4cabd6afbb95ca965eb684fce4a/raw/3cc69f1ee8cc4440da4a722026aa862ea0a791b8/Promotions.json"
 }
 
 //https://gist.githubusercontent.com/palcalde/6c19259bd32dd6aafa327fa557859c2f/raw/ba51779474a150ee4367cda4f4ffacdcca479887/Products.json
