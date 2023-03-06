f47ed9f68605adbdc8df5e9d65a861f2cf7b4d09
rpenya <rpenya.mail@gmail.com>
2023-03-06 14:08:06 +0100

    tests wip

--- rpenya-eshop-challenge/Data/DataTests/Products/ProductResourcesTests.swift
+++ rpenya-eshop-challenge/Data/DataTests/Products/ProductResourcesTests.swift
@@ -8,9 +8,7 @@
 import XCTest
 
 final class ProductResourcesTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_getAllProductsResource_success() {
+        
     }
-    
 }
--- rpenya-eshop-challenge/Data/DataTests/Products/ProductsRemoteDataSourceTests.swift
+++ rpenya-eshop-challenge/Data/DataTests/Products/ProductsRemoteDataSourceTests.swift
@@ -8,9 +8,11 @@
 import XCTest
 
 final class ProductsRemoteDataSourceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_getAllProducts_success() {
+        
     }
-
+    
+    func test_getAllProducts_error() {
+        
+    }
 }
