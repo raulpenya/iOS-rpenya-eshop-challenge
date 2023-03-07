89f058d667638abb5e185653cd2e924f4f537f18
rpenya <rpenya.mail@gmail.com>
2023-03-07 18:14:34 +0100

    ProductsListView wip

--- rpenya-eshop-challenge/Mobile/ProductsList/ProductsListView.swift
+++ rpenya-eshop-challenge/Mobile/ProductsList/ProductsListView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Domain
 
 struct ProductsListView: View {
     @ObservedObject var viewModel: ProductsListViewModel
     
     var body: some View {
-        Text("ProductsListView")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(0..<30) { _ in
+                        let promotion = Promotion(productCode: "TSHIRT", name: "Pay 1 get 2", type: .percentage, unitsNeeded: 2, discount: 0.5)
+                        let product = Product(code: "TSHIRT", name: "Cabify T-Shirt", price: 20, promotion: promotion)
+                        ProductListCell(item: product)
+                    }
+                }.listStyle(PlainListStyle())
+                CheckoutButtonView()
+            }.navigationTitle("eShop")
+        }
     }
 }
 
---
+++ rpenya-eshop-challenge/Mobile/ProductsList/UI/Views/CheckoutButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  CheckoutButtonView.swift
+//  rpenya-eshop-challenge
+//
+//  Created by raulbot on 7/3/23.
+//
+
+import SwiftUI
+
+struct CheckoutButtonView: View {
+    var body: some View {
+        HStack {
+            Button("Proceed checkout") {
+                print("Button -")
+            }.frame(height: 70).frame(minWidth: 0, maxWidth: .infinity)
+                .background {
+                    RoundedRectangle(cornerRadius: 12.0)
+                        .fill(Color(.systemOrange))
+                }
+        }
+        .padding(.all, 20).frame(maxWidth: .infinity, alignment: .center).background(Color.white // any non-transparent background
+            
+            .shadow(color: .black.opacity(0.3), radius: 3, x: 0, y: -5)//.mask(Rectangle().padding(.top, -20))
+          )
+    }
+}
+
+struct CheckoutButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutButtonView()
+    }
+}
