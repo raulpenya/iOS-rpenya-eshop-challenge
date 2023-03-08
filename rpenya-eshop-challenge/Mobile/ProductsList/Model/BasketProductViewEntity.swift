a6fbf48bfea78ba947c90ae57eafd7b0bd374a11
rpenya <rpenya.mail@gmail.com>
2023-03-08 14:40:28 +0100

    transform to basket entities added

--- rpenya-eshop-challenge/Mobile/ProductsList/Model/BasketProductViewEntity.swift
+++ rpenya-eshop-challenge/Mobile/ProductsList/Model/BasketProductViewEntity.swift
@@ -6,8 +6,21 @@
 //
 
 import Foundation
+import Domain
 
 struct BasketProductViewEntity {
     let product: ProductViewEntity
     let units: Int
+}
+
+extension BasketProductViewEntity {
+    func transformToProductList(action: ((ProductListItem, ProductListItemAction) -> Void)?) -> ProductListItem {
+        return ProductListItem(basketProduct: self, view: .productsList, action: action)
+    }
+}
+
+extension Product {
+    func transformToBasketProduct() -> BasketProductViewEntity {
+        return BasketProductViewEntity(product: transformToUI(), units: 0)
+    }
 }
--- rpenya-eshop-challenge/Mobile/ProductsList/Model/BasketViewEntity.swift
+++ rpenya-eshop-challenge/Mobile/ProductsList/Model/BasketViewEntity.swift
@@ -6,7 +6,20 @@
 //
 
 import Foundation
+import Domain
 
 struct BasketViewEntity {
     let products: [BasketProductViewEntity]
+}
+
+extension BasketViewEntity {
+    func transformToProductsList(action: ((ProductListItem, ProductListItemAction) -> Void)?) -> ProductsListItem {
+        return ProductsListItem(items: products.compactMap { $0.transformToProductList(action: action) })
+    }
+}
+
+extension Products {
+    func transformToBasket() -> BasketViewEntity {
+        return BasketViewEntity(products: products.compactMap { $0.transformToBasketProduct() })
+    }
 }
