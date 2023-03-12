1ae56f2dd12f2262202787ea1b46e196d5b54f1a
rpenya <rpenya.mail@gmail.com>
2023-03-12 16:34:50 +0100

    ProductsListViewModelTests done

---
+++ rpenya-eshop-challenge/Mobile/MobileTests/ProductsList/ProductsListRouterTests.swift
@@ -0,0 +1,14 @@
+//
+//  ProductsListRouterTests.swift
+//  rpenya-eshop-challengeTests
+//
+//  Created by raulbot on 11/3/23.
+//
+
+import XCTest
+
+final class ProductsListRouterTests: XCTestCase {
+    func test_destination() {
+        //
+    }
+}
