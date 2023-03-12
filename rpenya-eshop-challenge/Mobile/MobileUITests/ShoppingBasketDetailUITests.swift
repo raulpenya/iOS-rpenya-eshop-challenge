fe158d6697eda970031b6ead7011d8a8be0e2330
rpenya <rpenya.mail@gmail.com>
2023-03-12 20:00:32 +0100

    ShoppingBasketDetailUITests done

--- rpenya-eshop-challenge/Mobile/MobileUITests/ShoppingBasketDetailUITests.swift
+++ rpenya-eshop-challenge/Mobile/MobileUITests/ShoppingBasketDetailUITests.swift
@@ -8,34 +8,52 @@
 import XCTest
 
 final class ShoppingBasketDetailUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    private var app: XCUIApplication!
+    
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDown() {
+        app = nil
+    }
+    
+    func waitForShoppingBasketList() {
+        let predicate = NSPredicate(format: "identifier == 'products_list'")
+        let list = app.collectionViews.containing(predicate).element(boundBy: 0)
+        XCTAssertTrue(list.waitForExistence(timeout: 5))
+        XCTAssertEqual(list.children(matching: .cell).count, 3)
+        let predicateButton = NSPredicate(format: "identifier == 'product_list_complete_button'")
+        let button = app.buttons.containing(predicateButton).element(boundBy: 0)
+        let buttonAdd = app.collectionViews.children(matching: .cell).element(boundBy: 0).buttons["+"]
+        buttonAdd.tap()
+        button.tap()
+        let predicate3 = NSPredicate(format: "identifier == 'shopping_basket_list'")
+        let shoppingBasketList = app.collectionViews.containing(predicate3).element(boundBy: 0)
+        XCTAssertTrue(shoppingBasketList.waitForExistence(timeout: 2))
+    }
+    
+    func test_cancelButton() {
+        waitForShoppingBasketList()
+        let predicateButton = NSPredicate(format: "identifier == 'cancel_button'")
+        let button = app.buttons.containing(predicateButton).element(boundBy: 0)
+        button.tap()
+        let predicateShoppingbasket = NSPredicate(format: "identifier == 'shopping_basket_list'")
+        let shoppingBasketList = app.collectionViews.containing(predicateShoppingbasket).element(boundBy: 0)
+        XCTAssertFalse(shoppingBasketList.waitForExistence(timeout: 2))
+    }
+    
+    func test_completebutton() {
+        waitForShoppingBasketList()
+        let predicateButton = NSPredicate(format: "identifier == 'shopping_basket_complete_button'")
+        let button = app.buttons.containing(predicateButton).element(boundBy: 0)
+        button.tap()
+        let alert = app.otherElements.children(matching: .alert).element(boundBy: 0)
+        alert.buttons.element(boundBy: 0).tap()
+        let predicateShoppingbasket = NSPredicate(format: "identifier == 'shopping_basket_list'")
+        let shoppingBasketList = app.collectionViews.containing(predicateShoppingbasket).element(boundBy: 0)
+        XCTAssertFalse(shoppingBasketList.waitForExistence(timeout: 2))
     }
 }
