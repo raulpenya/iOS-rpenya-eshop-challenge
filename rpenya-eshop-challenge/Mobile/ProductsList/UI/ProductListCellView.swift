1a808da2721044e44ac319c7584c6227eb3c13e6
rpenya <rpenya.mail@gmail.com>
2023-03-13 13:46:51 +0100

    AccessibilityIdentifierConstants added

--- rpenya-eshop-challenge/Mobile/ProductsList/UI/ProductListCellView.swift
+++ rpenya-eshop-challenge/Mobile/ProductsList/UI/ProductListCellView.swift
@@ -30,7 +30,7 @@ struct ProductListCellView: View {
                         AddRemoveButton(title: "-") {
                             item.action(item, .remove)
                         }
-                        Text(item.getUnits()).modifier(UnitsTextModifier()).frame(width: 30, height: 30).accessibilityIdentifier("unit_text")
+                        Text(item.getUnits()).modifier(UnitsTextModifier()).frame(width: 30, height: 30).accessibilityIdentifier(AccessibilityIdentifierConstants.unitText)
                         AddRemoveButton(title: "+") {
                             item.action(item, .add)
                         }
